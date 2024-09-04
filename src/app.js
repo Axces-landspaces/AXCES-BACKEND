0d15a4a33444f303031b6a3349968a74e649bd89
Anshul Wadhwa <anshulwadhwa439@gmail.com>
2024-09-04 17:39:15 +0530

    Update cors config

--- src/app.js
+++ src/app.js
@@ -15,7 +15,7 @@ const app = express();
 
 // CORS configuration
 const corsOptions = {
-  origin: 'https://axces.in',
+  origin: ['https://axces.in', 'http://localhost:5000'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
